b3f65f641ec3a1789b42eb54968ad8597af6cce1
Hanif415 <hanifamrullah42@gmail.com>
2021-01-31 10:40:41 +0700

    configure search view

--- app/src/main/java/com/koshka/githubuser/HomeFragment.kt
+++ app/src/main/java/com/koshka/githubuser/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.koshka.githubuser
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
+    lateinit var searchView : SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,4 +21,35 @@ class HomeFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        searchView = view.findViewById(R.id.search_view) as SearchView
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        searchView.setOnQueryTextListener(this)
+        searchView.queryHint = getString(R.string.search_hint)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search_view).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+        }
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        Toast.makeText(activity, p0, Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
